2c7e9febecb7500b33920467bdd3f6f13487accb
elian2310 <elianpacheco23.10@gmail.com>
2023-09-14 16:41:06 -0400

    Finished until vector storage

--- musicbot.py
+++ musicbot.py
@@ -5,8 +5,7 @@ import streamlit as st
 import rdflib
 import rdflib.plugins.stores.sparqlstore as store
 from dotenv import load_dotenv, find_dotenv
-from langchain.document_loaders import PyPDFLoader
-from langchain.document_loaders.csv_loader import CSVLoader
+from langchain.document_loaders import PyPDFLoader, PyPDFDirectoryLoader, DirectoryLoader, CSVLoader
 from langchain.text_splitter  import RecursiveCharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
@@ -16,47 +15,49 @@ from chromadb.api.types import Embeddings
 from fastapi import FastAPI
 
 
-os.environ["OPENAI_API_KEY"] = "openIAKEY"
+os.environ["OPENAI_API_KEY"] = "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"
 _ = load_dotenv(find_dotenv()) # read local .env file
 openai.api_key = os.environ['OPENAI_API_KEY']
 
 # LOAD DATA
 
-csvs = 10
-pdfs = 10
+pdfLoader = PyPDFDirectoryLoader("./data/PDF/")
+csvLoader = DirectoryLoader('./data/CSV/', glob='**/*.csv', loader_cls=CSVLoader, loader_kwargs={'encoding': 'latin-1'})
 
-csvLoaders = []
-pdfLoaders = []
-
-for i in range(csvs):
-  csvLoaders.append(CSVLoader(file_path=f"./data/CSV/csv{i}.csv", encoding="utf-8", csv_args={'delimiter': ','}))
-
-for i in range(pdfs):
-  pdfLoaders.append(PyPDFLoader(f"./data/PDF/pdf{i}.pdf"))
-
-loadedCsv = []
-loadedPdf = []
-
-for i in range(len(csvLoaders)):
-  loadedCsv.append(csvLoaders[i].load())
-
-for i in range(len(pdfLoaders)):
-  loadedPdf.append(pdfLoaders[i].load())
+loadedPdf = pdfLoader.load()
+loadedCsv = csvLoader.load()
 
 # SPLIT DATA
 
-splitter = RecursiveCharacterTextSplitter(chunk_size = 300, chunk_overlap = 30, separators = "\n")
+pdfSplitter = RecursiveCharacterTextSplitter(chunk_size = 300, chunk_overlap = 30, separators = "\n")
+csvSplitter = RecursiveCharacterTextSplitter(chunk_size = 400, chunk_overlap = 0)
 
-pdfSplits = splitter.split_documents(loadedPdf)
+pdfSplits = pdfSplitter.split_documents(loadedPdf)
+csvSplits = csvSplitter.split_documents(loadedCsv)
 
 # VECTOR STORAGE
 
-embeddings = OpenAIEmbeddings()
+embeddings = OpenAIEmbeddings(openai_api_key=os.environ['OPENAI_API_KEY'])
 
-db_directory = 'docs/chroma'
+db_directory = './docs/chroma'
 
-vectordb = Chroma(embeddings=embeddings, persist_directory=db_directory)
-vectordb.add_documents(documents=pdfSplits)
-vectordb.add_documents(documents=loadedCsv)
+vectordb = Chroma(embedding_function=embeddings, persist_directory=db_directory)
+
+batch_size = 5000
+
+print(len(pdfSplits))
+print(len(csvSplits))
+
+for i in range(0, 10000, batch_size):
+    batch = pdfSplits[i:i+batch_size]  
+    vectordb.add_documents(documents=batch)
+
+vectordb.add_documents(pdfSplits[15000:])
+
+for i in range(0, 40000, batch_size):
+    batch = csvSplits[i:i+batch_size]
+    vectordb.add_documents(documents=batch)
+
+vectordb.add_documents(csvSplits[45000:])
 
 vectordb.persist()
